a3280823bfbd9022cc7fb3c0a89b8d2bc737f290
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-29 09:57:20 +0200

    /flashcards in

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express from 'express';
+import { getFlashcards } from './model.js';
 
 const app = express();
 const port = 3601;
 
 app.get('/', (req: express.Request, res: express.Response) => {
-	res.send('testing TypeScript222');
+	res.send(`
+<h1>Test API</h1>	
+<ul>
+	<li><a href="/flashcards">flashcards</a></li>
+</ul>
+	`);
+});
+
+app.get('/flashcards', (req: express.Request, res: express.Response) => {
+	res.json(getFlashcards());
 });
 
 app.listen(port, () => {
