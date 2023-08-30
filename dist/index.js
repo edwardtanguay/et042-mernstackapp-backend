2c6a8f6860d63316d674cc7fc4a882f8eabb7d70
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-30 09:15:01 +0200

    showing flashcards

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,19 @@
 import express from 'express';
+import { getFlashcards } from './model.js';
+import cors from 'cors';
 const app = express();
+app.use(cors());
 const port = 3601;
 app.get('/', (req, res) => {
-    res.send('testing TypeScript222');
+    res.send(`
+<h1>Test API</h1>	
+<ul>
+	<li><a href="/flashcards">flashcards</a></li>
+</ul>
+	`);
+});
+app.get('/flashcards', (req, res) => {
+    res.json(getFlashcards());
 });
 app.listen(port, () => {
     console.log(`listening on port http://localhost:${port}`);
